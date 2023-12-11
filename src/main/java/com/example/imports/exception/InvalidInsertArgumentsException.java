8d639206e59037af976c2fba2428a75dc6261222
Michal Fedor <55749697+fedor-michal@users.noreply.github.com>
2023-12-11 18:25:21 +0100

    Docker configuration

--- src/main/java/com/example/imports/exception/InvalidInsertArgumentsException.java
+++ src/main/java/com/example/imports/exception/InvalidInsertArgumentsException.java
@@ -1,7 +1,9 @@
 package com.example.imports.exception;
 
+import lombok.EqualsAndHashCode;
 import lombok.Value;
 
+@EqualsAndHashCode(callSuper = true)
 @Value
 public class InvalidInsertArgumentsException extends RuntimeException{
     String errors;
